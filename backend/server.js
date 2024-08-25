c192f946b880172d2ce15051bd8a0f81a8169ed8
Tiana <tianatanael12@gmail.com>
2024-08-25 09:42:56 +0700

    Added user APIs: signup, login, check-auth, logout, and other functions for debugging purposes

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+// Load env variables
+if (process.env.NODE_ENV != 'production') {
+    require("dotenv").config();
+}
+
+// Import dependencies
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./config/connectToDb");
+const usersController = require("./controllers/usersController");
+const requireAuth = require("./middleware/requireAuth");
+
+// Create an app object
+const app = express();
+
+// Config
+app.use(express.json());
+app.use(cookieParser());
+
+// Connect to database
+connectToDb();
+
+// Routes
+app.post("/signup", usersController.signup);
+app.post("/login", usersController.login);
+app.get("/check-auth", requireAuth, usersController.checkAuth)
+app.get("/logout", usersController.logout);
+
+// Debugging routes
+app.post("/debug/checkuser", usersController.checkUser);
+app.post("/debug/deluser", usersController.delUser);
+app.get("/debug/showall", usersController.showAllUsers);
+
+
+// Start server
+app.listen(process.env.PORT);
